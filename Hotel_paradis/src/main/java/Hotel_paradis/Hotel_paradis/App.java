51bbbcccc2d7819663a0cead4fb7b5cadb3d8077
cheikh123 <45607390+cheikh123@users.noreply.github.com>
2018-12-24 11:29:24 +0000

    commit Bye bye

--- Hotel_paradis/src/main/java/Hotel_paradis/Hotel_paradis/App.java
+++ Hotel_paradis/src/main/java/Hotel_paradis/Hotel_paradis/App.java
@@ -12,5 +12,6 @@ public class App
 		System.out.println( "Bienvenu" );
 		System.out.println( "Branch cheikh" );
 		System.out.println( "Branch mohameden" );
+		System.out.println( "Bye bye" );
     }
 }
